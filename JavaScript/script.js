beba4bb989b738f93296f2292018ec18ab285c5f
duynghiadev <duynghia22302@gmail.com>
2024-12-09 22:50:31 +0700

    5. Method array trong javascript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -46,3 +46,45 @@ const logName = (arr) => {
   });
 };
 logName(arr);
+
+// array
+const arr1 = ["html", "css"];
+arr1.forEach((item) => {
+  console.log("item:", item);
+});
+
+const result = arr1.map((item, index) => {
+  console.log("item.map:", item, index);
+});
+
+// filter
+const result2 = arr1.filter((item) => {
+  return item.length > 3;
+});
+console.log("result2:", result2);
+
+// reduce
+const result3 = arr1.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue.length;
+}, 0);
+console.log("result3:", result3);
+
+// sort
+const arr2 = [3, 1, 2, 5, 4];
+const result4 = arr2.sort((a, b) => {
+  return a - b;
+});
+console.log("result4:", result4);
+
+// spread operator
+const arr3 = [...arr1, "javascript", "react"];
+console.log("arr3:", arr3);
+console.log("arr1:", arr1);
+
+// rest operator
+function sum(...numbers) {
+  return numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+}
+console.log("sum:", sum(1, 2, 3, 4, 5));
