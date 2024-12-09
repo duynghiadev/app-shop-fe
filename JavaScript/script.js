700f07ac2250148b788f5649b139005672b4abed
duynghiadev <duynghia22302@gmail.com>
2024-12-09 22:12:35 +0700

    3. Hàm và tham số trong javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+const sumNumber = (a, b) => {
+  return a + b;
+};
+console.log("total:", sumNumber(1, 2));
+
+function sumNumber2(a, b) {
+  return a + b;
+}
+console.log("total:", sumNumber2(1, 5));
