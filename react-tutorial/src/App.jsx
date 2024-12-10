57ebc1cd9396640f389ba51e746f60cbfb518fab
duynghiadev <duynghia22302@gmail.com>
2024-12-10 14:11:26 +0700

    11. Tìm hiểu useEffect trong ReactJS

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import Todo from "./TodoList/Todo";
 
 function App() {
-  const count = 0;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("component re-render");
+    // cleanup functions
+    return () => {
+      console.log("component destroyed");
+    };
+  }, [count]);
+
   return (
     <>
       <Card initialCount={count} />
