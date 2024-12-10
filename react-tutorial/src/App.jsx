69ce8f842031e9ab5a7e0feafd4cbaffc6410c80
duynghiadev <duynghia22302@gmail.com>
2024-12-10 14:19:23 +0700

    12. Tìm hiểu useMemo trong ReactJS

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,9 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Card from "./Card";
 import Todo from "./TodoList/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
+  const course = [
+    {
+      name: "JavaScript",
+      score: 9,
+    },
+    {
+      name: "HTML",
+      score: 8,
+    },
+    {
+      name: "CSS",
+      score: 7,
+    },
+    {
+      name: "React",
+      score: 6,
+    },
+    {
+      name: "Node.js",
+      score: 5,
+    },
+  ];
 
   useEffect(() => {
     console.log("component re-render");
@@ -13,8 +35,15 @@ function App() {
     };
   }, [count]);
 
+  const totalScore = useMemo(() => {
+    course.reduce((total, current) => {
+      return (total += current.score);
+    }, 0);
+  }, []);
+
   return (
     <>
+      <h1>{totalScore}</h1>
       <Card initialCount={count} />
       <Todo />
     </>
