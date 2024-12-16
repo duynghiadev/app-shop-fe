8e3b325e697d5f861109db2214fd1d53b965dcf4
duynghiadev <duynghia22302@gmail.com>
2024-12-16 12:18:29 +0700

    23. PATCH method trong NextJS 14 (Rest API)

--- intro-nextjs14/src/app/products/api/[productId]/route.ts
+++ intro-nextjs14/src/app/products/api/[productId]/route.ts
@@ -27,3 +27,40 @@ export function GET(req: Request, context: any) {
     }
   );
 }
+
+export async function PATCH(req: Request, context: any) {
+  const productId = context?.params?.productId;
+  const indexUpdated = listProducts.findIndex(
+    (item) => item.id === parseInt(productId)
+  );
+  const updateProduct = await req.json();
+  console.log({ updateProduct });
+
+  if (indexUpdated !== -1) {
+    listProducts[indexUpdated].name = updateProduct.name
+      ? updateProduct.name
+      : listProducts[indexUpdated].name;
+    listProducts[indexUpdated].price = updateProduct.price
+      ? updateProduct.price
+      : listProducts[indexUpdated].price;
+
+    return NextResponse.json(
+      {
+        data: {
+          message: "Product updated",
+          data: listProducts[indexUpdated],
+        },
+      },
+      { status: 200, statusText: "Success" }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      data: {
+        message: "Product not found",
+      },
+    },
+    { status: 400, statusText: "Error" }
+  );
+}
