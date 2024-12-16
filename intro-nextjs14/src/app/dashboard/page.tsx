e5c4d9f91d8af00475cc588ff10d3a19ec30c85d
duynghiadev <duynghia22302@gmail.com>
2024-12-16 16:07:07 +0700

    32. Static Rendering trong NextJS

--- intro-nextjs14/src/app/dashboard/page.tsx
+++ intro-nextjs14/src/app/dashboard/page.tsx
@@ -3,5 +3,10 @@ import styles from "../page.module.css";
 export default function Index() {
   console.log("dashboard client component");
 
-  return <h1 className={styles.main}>Dashboard Page - client component</h1>;
+  return (
+    <>
+      <h1 className={styles.main}>Dashboard Page - client component</h1>
+      <h2>Time: {new Date().toLocaleTimeString()}</h2>
+    </>
+  );
 }
