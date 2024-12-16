d29dfa96f73a2dcbb1628ad6fc15a6ef904e82b6
duynghiadev <duynghia22302@gmail.com>
2024-12-16 11:27:38 +0700

    22. POST method trong NextJS 14 (Rest API)

--- intro-nextjs14/src/app/products/api/route.ts
+++ intro-nextjs14/src/app/products/api/route.ts
@@ -8,8 +8,32 @@ export function GET(req: NextRequest) {
   return NextResponse.json(
     { data },
     {
-      status: 201,
+      status: 200,
       statusText: "Success",
     }
+  );
+}
+
+export async function POST(req: NextRequest) {
+  const newProduct = await req.json();
+  const isExisted = listProducts.find((item) => item.name === newProduct.name);
+
+  if (!isExisted) {
+    const id = Math.max(...listProducts.map((item) => item.id)) + 1;
+    listProducts.push({
+      ...newProduct,
+      id,
+    });
+
+    const data = listProducts;
+    return NextResponse.json(
+      { data: { ...newProduct, id }, message: "Created Success" },
+      { status: 200, statusText: "Success" }
+    );
+  }
+
+  return NextResponse.json(
+    { data: null, message: "Product name is existed" },
+    { status: 400, statusText: "Invalid" }
   );
 }
