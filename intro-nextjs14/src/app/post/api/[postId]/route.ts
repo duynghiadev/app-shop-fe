402675453292c166fe2815ab5a9d87014656a974
duynghiadev <duynghia22302@gmail.com>
2024-12-16 18:00:23 +0700

    5. Viết API xóa bài post

--- intro-nextjs14/src/app/post/api/[postId]/route.ts
+++ intro-nextjs14/src/app/post/api/[postId]/route.ts
@@ -56,7 +56,7 @@ export async function PUT(
       );
     }
 
-    const existedTitle = await Post.findOne({ title });
+    const existedTitle = await Post.findOne({ title, _id: { $ne: id } });
     if (!existedTitle) {
       const updated = await Post.findByIdAndUpdate(
         id,
@@ -79,6 +79,44 @@ export async function PUT(
         message: "The title post is duplicated.",
       },
       { status: 400, statusText: "Invalid" }
+    );
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      {
+        data: null,
+        message: "Error",
+      },
+      { status: 500, statusText: "Failed" }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { postId: string } }
+) {
+  try {
+    const id = context.params.postId;
+    const post = await Post.findById(id);
+    if (!post) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "The post is not existed.",
+        },
+        { status: 400, statusText: "Failed" }
+      );
+    }
+
+    const deletedPost = await Post.findByIdAndDelete(id);
+
+    return NextResponse.json(
+      {
+        data: deletedPost,
+        message: "Success",
+      },
+      { status: 201 }
     );
   } catch (error) {
     console.log({ error });
