fa5f657b354ff8074d47e74dedd669ed1a3d6369
duynghiadev <duynghia22302@gmail.com>
2024-12-16 14:38:35 +0700

    27. Cookies trong route handlers

--- intro-nextjs14/src/app/my-profile/api/route.ts
+++ intro-nextjs14/src/app/my-profile/api/route.ts
@@ -1,20 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export function GET(req: NextRequest) {
-  // case 1
+  // case 1: take the token from the request
   const reqHeaders = new Headers(req.headers);
   const token = reqHeaders.get("Authorization");
   console.log("token:", { token });
 
-  // case 2
+  // case 2: take the token from the request
   const newHeaders = headers();
   const newToken = newHeaders.get("Authorization");
   console.log("new headers:", { newToken });
 
+  // case 3: take the cookie from the request
+  const reqCookies = cookies();
+  const myCookie = reqCookies.get("my-cookie");
+  console.log("req cookie:", { myCookie });
+
   return new NextResponse("<h1>My profile of me</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "my-cookie, city=hcm",
     },
   });
 }
